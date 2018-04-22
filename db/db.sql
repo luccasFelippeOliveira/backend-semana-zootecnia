ef4ad3906558ca58139c07a04e68cef86c3d0cdb
Luccas F. Oliveira <lfelippeoliveira@gmail.com>
2018-04-22 16:34:55 -0300

    atualiza db

--- db/db.sql
+++ db/db.sql
@@ -23,7 +23,8 @@ CREATE TABLE minicurso (
     horario_comeco datetime,
     horario_fim datetime,
     vagas integer,
-    vagas_restantes integer
+    vagas_restantes integer,
+    qtd_horas integer
 );
 
 DROP TABLE IF EXISTS participante_minicurso;
@@ -37,26 +38,32 @@ CREATE TABLE participante_minicurso (
     ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
+DROP TABLE IF EXISTS usuario;
+CREATE TABLE usuario (
+    usuario_id integer PRIMARY KEY AUTOINCREMENT,
+    username text,
+    pass text
+);
+
 INSERT INTO curso(nome) values  ('Zootecnia'),
                                 ('Eng. Agronômica'),
                                 ('Tecnologia em Alimentos'),
                                 ('Lic. Química'),
                                 ('Lic. Ciências Biológicas');
 
-INSERT INTO minicurso(nome, palestrante, horario_comeco, horario_fim, vagas, vagas_restantes) values
-    ('Prática comportamental em cães: avaliando situações de risco e reduzindo o estresse durante as manipulações', 'Ana Paula Ribeiro', '2018-05-16 07:30', '2018-05-16 11:30', 25, 25),
-    ('Leite: composição x manejo', 'Bento José Ribeiro', '2018-05-16 07:30', '2018-05-16 11:30', 30, 30),
-    ('Ferramenta prática para formulação de ração (Turma 1)', 'Flávio Salvador', '2018-05-16 07:30', '2018-05-16 11:30', 20, 20),
-    ('Boas práticas na colheita, extração e beneficiamento do mel', 'José Antônio Bessa', '2018-05-16 07:30', '2018-05-16 11:30', 25, 25),
-    ('Prática comportamental em cães: avaliando situações de risco e reduzindo o estresse durante as manipulações', 'Ana Paula Ribeiro', '2018-05-16 12:30', '2018-05-16 16:30', 25, 25),
-    ('Manejo reprodutivo de éguas', '', '2018-05-16 12:30', '2018-05-16 16:30', 25, 25),
-    ('Ferramenta prática para formulação de ração (Turma 2)', 'Flávio Salvador', '2018-05-16 12:30', '2018-05-16 16:30', 20, 20),
-    ('Inseminação artificial em perus', 'Francisco Ailton Batista - BRFoods', '2018-05-16 12:30', '2018-05-16 16:30', 25, 25),
-    ('Produção de derivados lácteos', 'Marlene Jerônimo', '2018-05-17 07:30', '2018-05-17 11:30', 20, 20),
-    ('Manejo geral na criação de ovinos', 'Sarita Bonagurio Gallo', '2018-05-17 07:30', '2018-05-17 11:30', 25, 25),
-    ('Manejo de animais silvestres', 'Lucas Andrade Carneiro- diretor de alimentação e nutrição de animais no zoológico de Brasília', '2018-05-17 07:30', '2018-05-17 11:30', 25, 25),
-    ('Sistema de suínos criados ao ar livre – SISCAL', '– professor na Faculdades Associadas de Uberaba (FAZU) e Universidade de Uberaba (UNIUBE)', '2018-05-17 07:30', '2018-05-17 11:30', 25, 25),
-    ('Produção de derivados lácteos', 'Marlene Jerônimo', '2018-05-17 12:30', '2018-05-17 16:30', 20, 20),
-    ('Manejo geral na criação de ovinos', 'Sarita Bonagurio Gallo', '2018-05-17 12:30', '2018-05-17 16:30', 25, 25),
-    ('Avaliação da qualidade de carnes', 'Lucas Arantes Pereira', '2018-05-17 12:30', '2018-05-17 16:30', 25, 25),
-    ('Forragicultura', '', '2018-05-17 12:30', '2018-05-17 16:30', 25, 25);
+INSERT INTO minicurso(nome, palestrante, horario_comeco, horario_fim, vagas, vagas_restantes, qtd_horas) values
+    ('Prática comportamental em cães: avaliando situações de risco e reduzindo o estresse durante as manipulações (8h)', 'Ana Paula Ribeiro', '2018-05-16 07:30', '2018-05-16 16:30', 25, 25), 8,
+    ('Leite: composição x manejo', 'Bento José Ribeiro', '2018-05-16 07:30', '2018-05-16 11:30', 30, 30, 4),
+    ('Ferramenta prática para formulação de ração (Turma 1)', 'Flávio Salvador', '2018-05-16 07:30', '2018-05-16 11:30', 20, 20,4 ),
+    ('Boas práticas na colheita, extração e beneficiamento do mel', 'José Antônio Bessa', '2018-05-16 07:30', '2018-05-16 11:30', 25, 25, 4),
+    ('Manejo reprodutivo de éguas', '', '2018-05-16 12:30', '2018-05-16 16:30', 25, 25, 4),
+    ('Ferramenta prática para formulação de ração (Turma 2)', 'Flávio Salvador', '2018-05-16 12:30', '2018-05-16 16:30', 20, 20, 4),
+    ('Inseminação artificial em perus', 'Francisco Ailton Batista - BRFoods', '2018-05-16 12:30', '2018-05-16 16:30', 25, 25, 4),
+    ('Produção de derivados lácteos', 'Marlene Jerônimo', '2018-05-17 07:30', '2018-05-17 16:30', 20, 20, 8),
+    ('Manejo geral na criação de ovinos', 'Sarita Bonagurio Gallo', '2018-05-17 07:30', '2018-05-17 16:30', 25, 25, 8),
+    ('Manejo de animais silvestres', 'Lucas Andrade Carneiro- diretor de alimentação e nutrição de animais no zoológico de Brasília', '2018-05-17 07:30', '2018-05-17 11:30', 25, 25, 4),
+    ('Sistema de suínos criados ao ar livre – SISCAL', '– professor na Faculdades Associadas de Uberaba (FAZU) e Universidade de Uberaba (UNIUBE)', '2018-05-17 07:30', '2018-05-17 11:30', 25, 25, 4),
+    ('Avaliação da qualidade de carnes', 'Lucas Arantes Pereira', '2018-05-17 12:30', '2018-05-17 16:30', 25, 25, 4),
+    ('Forragicultura', '', '2018-05-17 12:30', '2018-05-17 16:30', 25, 25, 4);
+
+INSERT INTO usuario(username, pass) values ('admin', 'admin');
--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/labstack/echo"
@@ -13,26 +14,35 @@ import (
 )
 
 type Curso struct {
-	ID   int    `json: "id"`
-	Nome string `json: "nome"`
+	ID   int    `json:"id"`
+	Nome string `json:"nome"`
 }
 
 type Minicurso struct {
-	ID             int       `json: "id"`
-	Nome           string    `json: "nome"`
-	Palestrante    string    `json: "palestrante"`
-	HorarioComeco  time.Time `json: "horarioComeco"`
-	HorarioFim     time.Time `json:"horarioFim"`
-	Vagas          int       `json:"vagas"`
-	VagasRestantes int       `json: "vagasRestantes"`
+	ID              int       `json:"id"`
+	Nome            string    `json:"nome"`
+	Palestrante     string    `json:"palestrante"`
+	HorarioComeco   time.Time `json:"horarioComeco"`
+	HorarioFim      time.Time `json:"horarioFim"`
+	Vagas           int       `json:"vagas"`
+	VagasRestantes  int       `json:"vagasRestantes"`
+	QuantidadeHoras int       `json:"quantidadeHoras"`
 }
 
 type Participante struct {
-	ID          int    `json: "id"`
-	CpfRa       int    `json: "cpf_ra"`
-	CursoID     int    `json: "curso_id"`
-	Nome        string `json: "nome"`
-	Instituicao string `json: "instituicao"`
+	ID          int    `json:"id"`
+	CpfRa       int    `json:"cpf_ra"`
+	CursoID     int    `json:"curso_id"`
+	Nome        string `json:"nome"`
+	Instituicao string `json:"instituicao"`
+}
+
+type ParticpanteRequest struct {
+	CpfRa       string      `json:"cpf_ra"`
+	Nome        string      `json:"nome"`
+	Instituicao string      `json:"instituicao"`
+	Curso       Curso       `json:"curso"`
+	Minicrusos  []Minicurso `json:"minicursos"`
 }
 
 var (
@@ -60,20 +70,8 @@ func dbGetCursos() []Curso {
 	return cursos
 }
 
-func dbGetMinicurso(dataInicio *int, dataFim *int) []Minicurso {
-	var rows *sql.Rows
-	var err error
-
-	if dataInicio == nil {
-		rows, err = db.Query("SELECT * FROM minicurso")
-	} else {
-		rows, err =
-			db.Query("SELECT * FROM minicurso"+
-				" where horario_comeco > datetime(?, \"unixepoch\", \"localtime\")"+
-				" AND horario_fim < datetime(?, \"unixepoch\", \"localtime\")"+
-				" AND vagas_restantes > 0", *dataInicio, *dataFim)
-	}
-
+func dbGetMinicurso() []Minicurso {
+	rows, err := db.Query("SELECT * FROM minicurso where vagas_restantes > 0")
 	if err != nil {
 		return nil
 	}
@@ -88,6 +86,7 @@ func dbGetMinicurso(dataInicio *int, dataFim *int) []Minicurso {
 		var horarioFim time.Time
 		var vagas int
 		var vagasRestantes int
+		var quantidadeHoras int
 
 		err := rows.Scan(
 			&id,
@@ -96,7 +95,8 @@ func dbGetMinicurso(dataInicio *int, dataFim *int) []Minicurso {
 			&horarioInicio,
 			&horarioFim,
 			&vagas,
-			&vagasRestantes)
+			&vagasRestantes,
+			&quantidadeHoras)
 
 		if err != nil {
 			fmt.Println("Cannot rows.Scan")
@@ -105,15 +105,216 @@ func dbGetMinicurso(dataInicio *int, dataFim *int) []Minicurso {
 
 		minicursos = append(minicursos,
 			Minicurso{
-				ID:             id,
-				Nome:           nome,
-				Palestrante:    palestrante,
-				HorarioComeco:  horarioInicio,
-				HorarioFim:     horarioFim,
-				Vagas:          vagas,
-				VagasRestantes: vagasRestantes})
+				ID:              id,
+				Nome:            nome,
+				Palestrante:     palestrante,
+				HorarioComeco:   horarioInicio,
+				HorarioFim:      horarioFim,
+				Vagas:           vagas,
+				VagasRestantes:  vagasRestantes,
+				QuantidadeHoras: quantidadeHoras})
 	}
 	return minicursos
+}
+
+func dbInsereParticipante(p *Participante) (int64, error) {
+	queryString := "INSERT INTO participante(cpf_ra, curso_id, nome, instituicao) values (?, ?, ?, ?)"
+
+	stmt, err := db.Prepare(queryString)
+	if err != nil {
+		return -1, err
+	}
+
+	res, err := stmt.Exec(p.CpfRa, p.CursoID, p.Nome, p.Instituicao)
+	if err != nil {
+		return -1, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return -1, err
+	}
+
+	return id, err
+}
+
+func dbAtualizaVaga(m *Minicurso) error {
+	query := "UPDATE minicurso SET vagas_restantes = vagas_restantes - 1	WHERE minicurso_id = ?"
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Stmt(stmt).Exec(m.ID)
+	if err != nil {
+		fmt.Println("Doing rollback dbAtualizaVaga")
+		tx.Rollback()
+	} else {
+		tx.Commit()
+	}
+
+	return nil
+}
+
+func dbInscreverMinicurso(m *Minicurso, p *Participante) error {
+	query := "INSERT INTO participante_minicurso(participante_id, minicurso_id) VALUES (?,?)"
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Stmt(stmt).Exec(p.ID, m.ID)
+	if err != nil {
+		fmt.Println("Doing rollback dbInscreverMinicurso")
+		tx.Rollback()
+	} else {
+		tx.Commit()
+	}
+
+	return nil
+}
+
+func dbRemoverInscricao(m *Minicurso, p *Participante) error {
+	query := "DELETE FROM participante_minicurso WHERE participante_id = ? AND minicurso_id = ?"
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Stmt(stmt).Exec(p.ID, m.ID)
+	if err != nil {
+		fmt.Println("Doing rollback dbRemoverInscricao")
+		tx.Rollback()
+	} else {
+		tx.Commit()
+	}
+
+	return nil
+}
+
+func dbGetParticipante(cpfRa int) (*Participante, error) {
+	query := "SELECT * FROM participante WHERE cpf_ra = ?"
+
+	rows, err := db.Query(query, cpfRa)
+	if err != nil {
+		return nil, err
+	}
+
+	participante := new(Participante)
+
+	defer rows.Close()
+	for rows.Next() {
+		var ID int
+		var CpfRa int
+		var CursoID int
+		var Nome string
+		var Instituicao string
+
+		err := rows.Scan(
+			&ID,
+			&CpfRa,
+			&CursoID,
+			&Nome,
+			&Instituicao)
+
+		if err != nil {
+			return nil, err
+		}
+
+		participante.ID = ID
+		participante.CpfRa = CpfRa
+		participante.CursoID = CursoID
+		participante.Nome = Nome
+		participante.Instituicao = Instituicao
+
+		return participante, nil
+	}
+
+	return nil, nil
+}
+
+func atualizaVagas(m *Minicurso, p *Participante) error {
+	err := dbInscreverMinicurso(m, p)
+	if err != nil {
+		return fmt.Errorf("Failed to inscrever minicurso")
+	}
+
+	err = dbAtualizaVaga(m)
+	if err != nil {
+		// Remove inscricao
+		if e := dbRemoverInscricao(m, p); e != nil {
+			return fmt.Errorf("Failed to remove inscricao")
+		}
+		return fmt.Errorf("Failed to atualizaVagas")
+	}
+	return nil
+}
+
+func novaInscricao(inscricao *ParticpanteRequest) error {
+	// Insere novo participante
+	p := Participante{}
+	p.Nome = inscricao.Nome
+
+	if (inscricao.Curso == Curso{}) {
+		p.CursoID = 0
+	} else {
+		p.CursoID = inscricao.Curso.ID
+	}
+
+	cpfRa, err := strconv.Atoi(inscricao.CpfRa)
+	if err != nil {
+		return err
+	}
+
+	p.CpfRa = cpfRa
+
+	id, err := dbInsereParticipante(&p)
+	if err != nil {
+		return err
+	}
+
+	p.ID = int(id)
+
+	// Inserir os minicursos
+	for _, minicurso := range inscricao.Minicrusos {
+		err = atualizaVagas(&minicurso, &p)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func verificaInscricao(inscricao *ParticpanteRequest) (bool, error) {
+	cpfRa, err := strconv.Atoi(inscricao.CpfRa)
+	if err != nil {
+		return false, err
+	}
+
+	participante, err := dbGetParticipante(cpfRa)
+	if err != nil {
+		return false, err
+	}
+
+	return participante != nil, nil
 }
 
 func GetCursos(c echo.Context) error {
@@ -126,33 +327,35 @@ func GetCursos(c echo.Context) error {
 }
 
 func GetMiniCursos(c echo.Context) error {
-	minicursos := dbGetMinicurso(nil, nil)
+	minicursos := dbGetMinicurso()
 	if minicursos != nil {
 		return c.JSON(http.StatusOK, minicursos)
 	}
 	return fmt.Errorf("Minicurso is null")
 }
 
-func GetMiniCursosData(c echo.Context) error {
-	// Pega horario maximo.
-	body := &struct {
-		DataInicio int
-		DataFim    int
-	}{}
+func PostNovaInscricao(c echo.Context) error {
+	inscricao := new(ParticpanteRequest)
 
-	if err := c.Bind(body); err != nil {
+	if err := c.Bind(inscricao); err != nil {
 		return err
 	}
 
-	minicursos := dbGetMinicurso(&body.DataInicio, &body.DataFim)
-	if minicursos != nil {
-		return c.JSON(http.StatusOK, minicursos)
+	existe, err := verificaInscricao(inscricao)
+	if err != nil {
+		fmt.Println("Could not verifica")
+		return c.String(http.StatusBadRequest, "Erro ao verificar Usuario")
 	}
 
-	return fmt.Errorf("minicursos is null")
-}
+	if existe {
+		return c.String(http.StatusForbidden, "Iscricao ja existe")
+	}
 
-func PostNovaInscricao(c echo.Context) error {
+	err = novaInscricao(inscricao)
+	if err != nil {
+		fmt.Println("Failed to inscrever")
+		return c.String(http.StatusBadRequest, "Erro ao realizar inscricao")
+	}
 
 	return nil
 }
@@ -183,7 +386,6 @@ func main() {
 
 	e.GET("/cursos", GetCursos)
 	e.GET("/minicursos", GetMiniCursos)
-	e.POST("/minicursos", GetMiniCursosData)
 	e.POST("/inscricao", PostNovaInscricao)
 
 	// Start Server
